f9f34a11a96160647ef1c87b616fcdbc870b7173
Mizobuchi <fastec0035@icloud.com>
2019-06-08 16:57:20 +0900

    mizobuchi

--- src/pages/UserRegistration.vue
+++ src/pages/UserRegistration.vue
@@ -1,7 +1,17 @@
 <template lang="html">
+<div>
   <div>
-    <p>UserRegistation</p>
+    <p>Name</p>
+    <input class="input has-text-light" type="text" placeholder="Name input">
+    <p>From</p>
+    <input class="input" type="text" placeholder="From input">
+    <p>To</p>
+    <input class="input" type="text" placeholder="To input">
+    <p>Line</p>
+    <input class="input" type="text" placeholder="Line input">
   </div>
+    <a class="button is-outlined is-large">Submit</a>
+</div>
 </template>
 
 <script>
